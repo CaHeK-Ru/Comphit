51353e261d2e371b561e03d40e1c0e57f6c02c4a
CaHeK-Ru <30568512+CaHeK-Ru@users.noreply.github.com>
2017-08-09 15:19:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Компьютерные хитрости</title>
+  <link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+  <meta name="description" content="Информационно-развлекательный портал для
+  программистов! Новости IT, советы и многое другое">
+  <meta name="keywords" content="курсы, новости, советы, хитрости, компьютеры">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="img/favicon.ico" rel="shortcut icon" type="image/x-icon"
+</head>
+<body>
+  <header>
+    <div id="logo">
+      <a href="https://comphit.com" title="На главную">
+        <img src="img/logo.png" title="Группа в ОК"
+alt="Comhit.com">
+        <span>CompHit</span>
+      </a>
+    </div>
+    <div id="about">
+      <a href="" title="Узнать детальнее об рекламе">Реклама</a>
+      <a href="" title="Написать нам письмо">Обратная связь</a>
+    </div>
+    <div id="reg_auth">
+      <a href="" title="Войти в личный кабинет пользователя">
+        <div id="btn">
+          Войти
+        </div>
+      </a>
+      <a href="" title="Зарегестрироваться на сайте">
+        <div id="btn">
+          Регистрация
+        </div>
+      </a>
+    </div>
+  </header>
+</body>
+</html>
